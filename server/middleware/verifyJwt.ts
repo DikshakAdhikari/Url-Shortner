6009ae161e85ed2db36341caabe4891e17006868
hunktuck <dikshak302000@gmail.com>
2023-12-23 17:08:57 +0530

    done with token verification

--- server/middleware/verifyJwt.ts
+++ server/middleware/verifyJwt.ts
@@ -1,9 +1,37 @@
 import jwt from 'jsonwebtoken'
 import express, { Request,Response, NextFunction } from 'express'
 
+const secret= 'secret'
 export const vertifyJwt = async(req:Request,res:Response,next:NextFunction)=> {
     try{
-        
+        const reqHeader = req.headers.authorization;
+        if(reqHeader){
+        const token = reqHeader?.split(' ')[1]
+        if(!process.env.SECRET_KEY){
+            return res.sendStatus(403);
+         }
+
+         jwt.verify(token, process.env.SECRET_KEY, (err, payload)=> {
+            if(err){
+                return res.status(400).json(err)
+            }
+            if(!payload){
+                return res.sendStatus(403)
+            }
+
+            if(typeof payload === "string"){
+                return res.sendStatus(403)
+            }
+
+            req.headers["userId"]= payload.id
+            next()
+
+         })
+       
+    }else{
+        res.sendStatus(401)
+    }
+
     }catch(err){
         res.status(403).json({message: 'Jwt verification failed'})
     }
