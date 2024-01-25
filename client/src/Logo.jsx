be0d91e47850493601636f82ba1768a8aa7e3052
hunktuck <ddikshakk@gmail.com>
2024-01-25 23:56:41 +0530

    implemented swipe

---
+++ client/src/Logo.jsx
@@ -0,0 +1,12 @@
+
+export const LeftLogo= ()=> {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="32"><path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z"/></svg>
+    )
+}
+
+export const RightLogo= ()=> {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="32"><path d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z"/></svg>
+    )
+}
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,29 +1,42 @@
-import { FunctionComponent } from "react";
+"use client"
+import { FunctionComponent, useState } from "react";
+import {LeftLogo, RightLogo} from '../Logo'
+
 
 interface DashboardProps {
     
 }
  
 const Dashboard: FunctionComponent<DashboardProps> = () => {
+
+    const [expand, setExpand]= useState(true)
+
+
     return (
         <div className=" flex  w-[100vw]  bg-yellow-300  fixed">
-            <div className="  h-[100vh] w-[20vw] bg-pink-300 ">
-              <div className=" ">
-                    hggf
+            <div className={`${expand ? 'w-[20vw]': 'w-fit'}  h-[100vh]  bg-pink-300 `}>
+                <div className=" flex flex-col gap-4 items-end cursor-pointer ">
+              {
+                expand ? <div onClick={()=> setExpand(false)}> <LeftLogo /></div>: <div onClick={()=> setExpand(true)}> <RightLogo /> </div>
+              }
               </div>
+              <div className="">fdfd</div>
             </div>
-            <div className=" flex w-[100vw] flex-col">
+            <div className=" flex w-[100vw] h-[100vh] flex-col">
             <div className=" max-h-screen  w-[100%] h-[7vh] bg-red-500">
                 hghgh
             </div>
-            <div className=" relative">
-                <div className=" flex overflow-scroll p-3 h-[100vh] flex-col gap-96">
+           
+                <div className=" flex overflow-auto  gap-36 p-10 h-[100vh] flex-col ">
+                    <div className=" z-20">tttttttttttt</div>
                     <div>gfgfg</div>
                     <div>gfgfg</div>
-                    <div>gfgfg</div>
-                    <div>gfgfg</div>
+                    <div>gggggggggg</div>
+                    <div>gggggggggg</div>
+                    <div>gggggggggg</div>
+                    <div>ggggggddddddddddddddddddddgggg</div>
                 </div>
-            </div>
+           
             
             </div>
         </div>
