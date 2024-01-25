13482fd3d5b96804c9546fd137d507c27747a397
hunktuck <ddikshakk@gmail.com>
2024-01-25 19:33:59 +0530

    ui initialized

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Dashboard from "@/components/Home";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <Dashboard />
+   </div>
+  );
+}
---
+++ client/src/components/Home.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+
+interface DashboardProps {
+    
+}
+ 
+const Dashboard: FunctionComponent<DashboardProps> = () => {
+    return (
+        <div>
+            hiiiiiiiiii
+        </div>
+      );
+}
+ 
+export default Dashboard;
