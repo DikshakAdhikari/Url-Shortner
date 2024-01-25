747762f4dd86063a39c56ed8315313cd51d53e50
hunktuck <ddikshakk@gmail.com>
2024-01-25 23:03:14 +0530

    nav brick added

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -6,8 +6,26 @@ interface DashboardProps {
  
 const Dashboard: FunctionComponent<DashboardProps> = () => {
     return (
-        <div>
-            hiiiiiiiiii
+        <div className=" flex  w-[100vw]  bg-yellow-300  fixed">
+            <div className="  h-[100vh] w-[20vw] bg-pink-300 ">
+              <div className=" ">
+                    hggf
+              </div>
+            </div>
+            <div className=" flex w-[100vw] flex-col">
+            <div className=" max-h-screen  w-[100%] h-[7vh] bg-red-500">
+                hghgh
+            </div>
+            <div className=" relative">
+                <div className=" flex overflow-scroll p-3 h-[100vh] flex-col gap-96">
+                    <div>gfgfg</div>
+                    <div>gfgfg</div>
+                    <div>gfgfg</div>
+                    <div>gfgfg</div>
+                </div>
+            </div>
+            
+            </div>
         </div>
       );
 }
