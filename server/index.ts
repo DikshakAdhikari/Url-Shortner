0cdf5757b065dcc8f0e754be349dc2abe88fc400
hunktuck <ddikshakk@gmail.com>
2024-02-02 14:00:27 +0530

    position changed

--- server/index.ts
+++ server/index.ts
@@ -6,21 +6,18 @@ import cors from 'cors'
 import dotenv from 'dotenv'
  dotenv.config()
  const port= process.env.PORT
+ import { mongooseConnect } from './connection/connect';
+ mongooseConnect()
  import cookieParser from 'cookie-parser'
 import { urlModel } from './models/url';
-import mongoose from 'mongoose';
  app.use(cors({
      origin: 'http://localhost:3001', 
      credentials: true, 
    })); 
-//@ts-ignore
-mongoose.connect(process.env.MONGO_URI).then(console.log('mongodb connected'))
+
  app.use(cookieParser())
  app.use(express.urlencoded({extended:false})) //It is used to handle form data as request
 app.use(express.json())
-
-
-  
 
 
 app.use('/url', urlRouter)
